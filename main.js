4b52f153cdefa722b5e10f95609ecbf6ec8e1d3f
LeviBreakwell <l.breakwell@me.com>
2023-02-04 22:26:28 +1000

    create program function

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 console.log("Hello World!")
+
+const reps = () => {
+    var sets = []   
+    for (var i = 0 ; i < 3 ; i++) {
+        sets.push(Math.floor(Math.random()*30+1))
+    }
+    return sets
+}
+const sets = Math.floor(Math.random()*8+1)
+   
+var exercises = ['Squats','Bench', 'Chin-ups', 'Lunge', 'RDL','Bi-curl',
+    'tri-extention','calf raise', 'KOT calf raise', 'Pistol-squat'
+     ]
+
+const createProgram = () => {
+    var program= []
+        for ( var i =0; i < sets ; i++) {
+            var exNum = Math.floor(Math.random()*10)
+            program.push(`${i+1}. ${reps()[0]} x 3 : ${exercises[exNum]}`)
+            exercises.filter((ex,i) => 
+                i != exNum
+            )
+        }
+    return program
+}
+
+console.log("Today's Program:")
+
+console.log(createProgram())
